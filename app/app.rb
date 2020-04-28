0948cca570f2161d62767301c8129107c532a53a
JD Harrington <jdharrington@instructure.com>
2020-04-28 17:09:36 -0400

    Throw an error if RUN_TRUSS_DEMO is not set

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,23 @@
 require 'sinatra/base'
+require 'sinatra/custom_logger'
 require 'sinatra/json'
+require 'logger'
 
 class TrussDemo < Sinatra::Base
   set :bind, '0.0.0.0'
 
+  helpers Sinatra::CustomLogger
+
+  configure :development, :production do
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+  end
+
   def self.run!
     unless ENV['RUN_TRUSS_DEMO']
+      logger.error("Environment variable RUN_TRUSS_DEMO must be set!")
+
       exit
     end
 
